784162c846af5d4dc8e0b02f5504754202fe7cad
cdk-king <1360531956@qq.com>
2019-02-14 06:02:29 +0800

    实现声音图像加载器和加载画面

--- js/game.js
+++ js/game.js
@@ -6,11 +6,14 @@ var game = {
     init:function(){
         //初始化对象
         levels.init();
+        loader.init();
+        
         //隐藏所有游戏图层，显示开始画面
         game.hideScreens();
         game.showScreen("gamestartscreen");
         game.canvas = document.getElementById("gamecanvas");
         game.context = game.canvas.getContext("2d");
+        //loader.loadSound("Neptune");
     },
     hideScreens: function() {
         var screens = document.getElementsByClassName("gamelayer");
@@ -72,5 +75,64 @@ var levels = {
     },
     load:function(number){
 
+    }
+}
+var loader = {
+    loaded:true,
+    loadedCount:0,//已加载的资源数
+    totalCount:0,//需要被加载的资源总数
+
+    init:function(){
+        //检查浏览器支持的声音格式
+        var mp3Support;
+        var oggSupport;
+        var audio = document.createElement("audio");
+        if(audio.canPlayType){
+            //当前canPlayType()方法返回“”、“maybe”、“probably”
+            mp3Support = "" != audio.canPlayType("audio/mpeg");
+            oggSupport = "" != audio.canPlayType("audio/ogg; codecs=\"vorbis\"");
+        }else{
+            //audio标签不支持
+            mp3Support = false;
+            oggSupport = false;
+        }
+        //检查ogg、mp3，如果都不支持，就将soundFileExtn设置成undefined
+        loader.soundFileExtn = mp3Support?".mp3":oggSupport?".ogg":undefined;
+    },
+    loadImage:function(url){
+        this.totalCount++;
+        this.loaded = false;
+        game.showScreen("loadingscreen");
+        var image = new Image();
+        image.src = url;
+        image.onload = loader.itemLoaded;
+        return image;
+    },
+    soundFileExtn:".mp3",
+    loadSound:function(url){
+        this.totalCount++;
+        this.loaded = false;
+        game.showScreen("loadingscreen");
+        var audio = new Audio();
+        audio.src = url +loader.soundFileExtn;
+        audio.addEventListener("canplaythrough",loader.itemLoaded,false);
+        return audio;
+    },
+    itemLoaded:function(ev){
+        // 加载此项目后，停止侦听其事件类型（加载或canPlayThrough）
+        ev.target.removeEventListener(ev.type, loader.itemLoaded, false);
+        loader.loadedCount++;
+        document.getElementById("loadingmessage").innerHTML = "Loaded " + loader.loadedCount + " of " + loader.totalCount;
+        if(loader.loadedCount == loader.totalCount){
+            //loader完成了资源加载
+            loader.loaded = true;
+            //隐藏加载页面
+            game.hideScreen("loadingscreen");
+            //如果loader.onload事件有响应函数，调用
+            if(loader.onload){
+                loader.onload();
+                loader.onload = undefined;
+            }
+        }
     }
 }
